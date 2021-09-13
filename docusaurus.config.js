1143ee2b586f6424ae14d914eeb7a57723c52804
oshiteku <oshiteku@gmail.com>
2021-09-14 03:08:40 +0900

    add: init docusaurus site

---
+++ docusaurus.config.js
@@ -0,0 +1,110 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'usukawa',
+  tagline: 'OBS向けオーバーレイシステム',
+  url: 'https://oshiteku.app/usukawa-docs/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'oshiteku',
+  projectName: 'obs-usukawa',
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'usukawa',
+        logo: {
+          alt: 'usukawa logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'チュートリアル',
+          },
+          {to: '/blog', label: 'ブログ', position: 'left'},
+          {
+            href: 'https://github.com/oshiteku/obs-usukawa',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'ドキュメント',
+            items: [
+              {
+                label: 'チュートリアル',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'コミュニティ',
+            items: [
+              {
+                label: 'Slack コミュニティ',
+                href: 'https://join.slack.com/t/oshiteku/shared_invite/zt-umby2fh2-Hrf0E5BZ789Kz9mHOM8ekQ',
+              },
+              {
+                label: '開発者 Twitter',
+                href: 'https://twitter.com/oshiteku',
+              },
+            ],
+          },
+          {
+            title: 'その他',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/oshiteku/obs-usukawa',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} oshiteku. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            ちょっと使ってみる (チュートリアル) - 5分 ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={"usukawa 公式ドキュメント"}
+      description="usukawa 公式ドキュメント。usukawa は拡張可能なOBS向けのオーバーレイシステムです。">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
